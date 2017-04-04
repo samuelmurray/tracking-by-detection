03169054133b8dc819c38c2419ede512ad3a9049
Samuel Murray <samuel.murray@outlook.com>
2017-04-04 14:57:06 +0900

    Change input format

--- cpp/src/examples/TrackDetections.cpp
+++ cpp/src/examples/TrackDetections.cpp
@@ -10,62 +10,61 @@
 using namespace std;
 
 static bool USE_GROUND_TRUTH = false;
-static bool USE_TEST_SET = false;
+static string DATASETS_DIR = "../data";
+static string RESULTS_DIR = "../data";
+const char *USAGE_MESSAGE = "Usage: %s [-g] [-s sequencePath] [-f sequencesFile]\n";
+const char *OPEN_FILE_MESSAGE = "Could not open file %s\n";
 
-void track(const string &sequence) {
-    string datasetsDir = "../data/2DMOT2015/data/";
-    string resultsDir = "../data/results/";
-    if (USE_TEST_SET) {
-        datasetsDir += "test/";
-        resultsDir += "test/";
-    } else {
-        datasetsDir += "train/";
-        resultsDir += "train/";
-    }
-
-    string inputFilePath = datasetsDir + sequence + "/";
-    string outputFilePath = resultsDir;
-    if (USE_GROUND_TRUTH) {
-        inputFilePath += "gt/gt.txt";
-        outputFilePath += "gt/" + sequence + ".txt";
-    } else {
-        inputFilePath += "det/det.txt";
-        outputFilePath += "det/" + sequence + ".txt";
-    }
-
-    ifstream inputStream(inputFilePath);
+void track(const string &inputPath, const string &outputPath) {
+    ifstream inputStream(inputPath);
     if (!inputStream.is_open()) {
-        cout << "Could not load file " << inputFilePath << endl;
-    } else {
-        ofstream outputStream;
-        outputStream.open(outputFilePath);
-        MCSORT tracker;
-        vector<Tracking> trackings;
-        for (auto const &detMap : DetectionFileParser::parseMOTFile(inputStream)) {
-            cout << "NEW FRAME - " << detMap.first << endl;
-            trackings = tracker.track(detMap.second);
-            cout << "---TRACKINGS---" << endl;
-            for (auto it = trackings.begin(); it != trackings.end(); ++it) {
-                cout << *it << endl;
-                outputStream << detMap.first << ","
-                             << it->ID << ","
-                             << it->bb.x1() << ","
-                             << it->bb.y1() << ","
-                             << it->bb.width << ","
-                             << it->bb.height << ","
-                             << "1,-1,-1,-1\n";
-            }
-            cout << endl;
-        }
-        outputStream.close();
+        fprintf(stderr, OPEN_FILE_MESSAGE, inputPath.c_str());
+        exit(EXIT_FAILURE);
     }
+    ofstream outputStream;
+    outputStream.open(outputPath);
+    if (!outputStream.is_open()) {
+        fprintf(stderr, OPEN_FILE_MESSAGE, outputPath.c_str());
+        exit(EXIT_FAILURE);
+    }
+    MCSORT tracker;
+    vector<Tracking> trackings;
+    for (auto const &detMap : DetectionFileParser::parseMOTFile(inputStream)) {
+        cout << "NEW FRAME - " << detMap.first << endl;
+        trackings = tracker.track(detMap.second);
+        cout << "---TRACKINGS---" << endl;
+        for (auto it = trackings.begin(); it != trackings.end(); ++it) {
+            cout << *it << endl;
+            outputStream << detMap.first << ","
+                         << it->ID << ","
+                         << it->bb.x1() << ","
+                         << it->bb.y1() << ","
+                         << it->bb.width << ","
+                         << it->bb.height << ","
+                         << "1,-1,-1,-1\n";
+        }
+        cout << endl;
+    }
+    outputStream.close();
 }
 
-const char *USAGE_MESSAGE = "Usage: %s [-g] [-s sequence] [-f sequencesFile]\n";
+void track(const string &sequenceName) {
+    string inputPath = DATASETS_DIR + "/" + sequenceName;
+    string outputPath = RESULTS_DIR;
+    if (USE_GROUND_TRUTH) {
+        inputPath += "/gt/gt.txt";
+        outputPath += "/gt/" + sequenceName + ".txt";
+    } else {
+        inputPath += "/det/det.txt";
+        outputPath += "/det/" + sequenceName + ".txt";
+    }
+    track(inputPath, outputPath);
+}
+
 
 int main(int argc, char **argv) {
     int opt;
-    string sequence;
+    string sequencePath;
     string sequencesFile;
     while ((opt = getopt(argc, argv, "gs:f:")) != -1) {
         switch (opt) {
@@ -73,7 +72,7 @@ int main(int argc, char **argv) {
                 USE_GROUND_TRUTH = true;
                 break;
             case 's':
-                sequence = optarg;
+                sequencePath = optarg;
                 break;
             case 'f':
                 sequencesFile = optarg;
@@ -83,28 +82,25 @@ int main(int argc, char **argv) {
                 exit(EXIT_FAILURE);
         }
     }
-    if (sequence != "" && sequencesFile != "") {
+    if (sequencePath != "" && sequencesFile != "") {
         fprintf(stderr, "You can't specify both -s and -f\n");
-    } else if (USE_GROUND_TRUTH && USE_TEST_SET) {
-        fprintf(stderr, "You can't use both -g and -t");
-    } else if (sequence != "") {
-        track(sequence);
+    } else if (sequencePath != "") {
+        track(sequencePath, "./result.txt");
         exit(EXIT_SUCCESS);
     } else if (sequencesFile != "") {
         ifstream detectionFile(sequencesFile);
         if (detectionFile.is_open()) {
-            string firstLine;
-            getline(detectionFile, firstLine);
-            if (firstLine != "train" && firstLine != "test") {
-                fprintf(stderr, USAGE_MESSAGE, argv[0]);
-            } else {
-                USE_TEST_SET = firstLine == "test";
-                while (getline(detectionFile, sequence)) { track(sequence); }
-                detectionFile.close();
-                exit(EXIT_SUCCESS);
-            }
+            string relInputPath;
+            string relOutputPath;
+            getline(detectionFile, relInputPath);
+            getline(detectionFile, relOutputPath);
+            DATASETS_DIR += "/" + relInputPath;
+            RESULTS_DIR += "/" + relOutputPath;
+            while (getline(detectionFile, sequencePath)) { track(sequencePath); }
+            detectionFile.close();
+            exit(EXIT_SUCCESS);
         }
-        fprintf(stderr, "Could not load file %s\n", sequencesFile.c_str());
+        fprintf(stderr, OPEN_FILE_MESSAGE, sequencesFile.c_str());
     }
 
     fprintf(stderr, USAGE_MESSAGE, argv[0]);
