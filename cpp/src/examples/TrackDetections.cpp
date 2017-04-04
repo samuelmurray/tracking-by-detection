2bd501a66fb18d227b13f4be2979980a42914cd5
Samuel Murray <samuel.murray@outlook.com>
2017-04-04 18:54:26 +0900

    Change input format

--- cpp/src/examples/TrackDetections.cpp
+++ cpp/src/examples/TrackDetections.cpp
@@ -6,16 +6,16 @@
 #include <string>
 #include <vector>
 #include <unistd.h>
+#include <chrono>
 
 using namespace std;
 
 static bool USE_GROUND_TRUTH = false;
-static string DATASETS_DIR = "../data";
-static string RESULTS_DIR = "../data";
-const char *USAGE_MESSAGE = "Usage: %s [-g] [-s sequencePath] [-f sequencesFile]\n";
+static string DATA_DIR = "../data";
+const char *USAGE_MESSAGE = "Usage: %s [-g] [-s sequencePath] [-f configFile]\n";
 const char *OPEN_FILE_MESSAGE = "Could not open file %s\n";
 
-void track(const string &inputPath, const string &outputPath) {
+int track(const string &inputPath, const string &outputPath) {
     ifstream inputStream(inputPath);
     if (!inputStream.is_open()) {
         fprintf(stderr, OPEN_FILE_MESSAGE, inputPath.c_str());
@@ -27,11 +27,15 @@ void track(const string &inputPath, const string &outputPath) {
         fprintf(stderr, OPEN_FILE_MESSAGE, outputPath.c_str());
         exit(EXIT_FAILURE);
     }
+    int cumulativeDuration = 0;
     MCSORT tracker;
     vector<Tracking> trackings;
     for (auto const &detMap : DetectionFileParser::parseMOTFile(inputStream)) {
         cout << "NEW FRAME - " << detMap.first << endl;
+        auto startTime = chrono::steady_clock::now();
         trackings = tracker.track(detMap.second);
+        auto duration = chrono::steady_clock::now() - startTime;
+        cumulativeDuration += chrono::duration_cast<chrono::milliseconds>(duration).count();
         cout << "---TRACKINGS---" << endl;
         for (auto it = trackings.begin(); it != trackings.end(); ++it) {
             cout << *it << endl;
@@ -46,11 +50,12 @@ void track(const string &inputPath, const string &outputPath) {
         cout << endl;
     }
     outputStream.close();
+    return cumulativeDuration;
 }
 
-void track(const string &sequenceName) {
-    string inputPath = DATASETS_DIR + "/" + sequenceName;
-    string outputPath = RESULTS_DIR;
+int track(const string &datasetsDir, const string &resultsDir, const string &sequenceName) {
+    string inputPath = datasetsDir + "/" + sequenceName;
+    string outputPath = resultsDir;
     if (USE_GROUND_TRUTH) {
         inputPath += "/gt/gt.txt";
         outputPath += "/gt/" + sequenceName + ".txt";
@@ -58,14 +63,14 @@ void track(const string &sequenceName) {
         inputPath += "/det/det.txt";
         outputPath += "/det/" + sequenceName + ".txt";
     }
-    track(inputPath, outputPath);
+    return track(inputPath, outputPath);
 }
 
 
 int main(int argc, char **argv) {
     int opt;
     string sequencePath;
-    string sequencesFile;
+    string configFile;
     while ((opt = getopt(argc, argv, "gs:f:")) != -1) {
         switch (opt) {
             case 'g':
@@ -75,32 +80,38 @@ int main(int argc, char **argv) {
                 sequencePath = optarg;
                 break;
             case 'f':
-                sequencesFile = optarg;
+                configFile = optarg;
                 break;
             default:
                 fprintf(stderr, USAGE_MESSAGE, argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
-    if (sequencePath != "" && sequencesFile != "") {
+    if (sequencePath != "" && configFile != "") {
         fprintf(stderr, "You can't specify both -s and -f\n");
     } else if (sequencePath != "") {
         track(sequencePath, "./result.txt");
         exit(EXIT_SUCCESS);
-    } else if (sequencesFile != "") {
-        ifstream detectionFile(sequencesFile);
+    } else if (configFile != "") {
+        string sequencesFilePath = DATA_DIR + "/" + configFile;
+        ifstream detectionFile(sequencesFilePath);
         if (detectionFile.is_open()) {
-            string relInputPath;
-            string relOutputPath;
-            getline(detectionFile, relInputPath);
-            getline(detectionFile, relOutputPath);
-            DATASETS_DIR += "/" + relInputPath;
-            RESULTS_DIR += "/" + relOutputPath;
-            while (getline(detectionFile, sequencePath)) { track(sequencePath); }
+            string line;
+            getline(detectionFile, line);
+            string datasetsDir = DATA_DIR + "/" + line;
+            getline(detectionFile, line);
+            string resultsDir = DATA_DIR + "/" + line;
+
+            int cumulativeDuration = 0;
+            while (getline(detectionFile, sequencePath)) {
+                cumulativeDuration += track(datasetsDir, resultsDir, sequencePath);
+            }
             detectionFile.close();
+            cout << "---FINISHED---" << endl;
+            cout << "Total duration: " << cumulativeDuration << "ms" << endl;
             exit(EXIT_SUCCESS);
         }
-        fprintf(stderr, OPEN_FILE_MESSAGE, sequencesFile.c_str());
+        fprintf(stderr, OPEN_FILE_MESSAGE, configFile.c_str());
     }
 
     fprintf(stderr, USAGE_MESSAGE, argv[0]);
