5fa06d2872f6ba5d4e6a95d3ea1ac5ea0444865f
Samuel Murray <samuel.murray@outlook.com>
2017-04-14 16:51:03 +0900

    Add PIL version to not require matplotlib

--- python/plot_trackings.py
+++ python/plot_trackings.py
@@ -1,20 +1,42 @@
 from __future__ import print_function
 
-import os.path
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.image as mpimg
-from skimage import io
-import time
 
-PATH_TO_DETECTIONS_FILE = "data/detections.txt"
-PATH_TO_IMAGES = "../data/okutama/seq-1_1_10/img1/"
-PATH_TO_OUTPUT = "data/images/"
+SEQUENCE = "seq-2_2_1"
+PATH_TO_DETECTIONS_FILE = f"../data/results/okutama/img1/{SEQUENCE}.txt"
+PATH_TO_IMAGES = f"../data/okutama/{SEQUENCE}/img1/"
+PATH_TO_OUTPUT = "../data/results/okutama/trackings/"
 
-def generate_and_save():
+
+def generate_and_save_PIL():
+  from PIL import Image, ImageDraw
+
   detections = np.loadtxt(PATH_TO_DETECTIONS_FILE, delimiter=",")
+  colours = np.random.randint(0, 255, (32, 3))
 
+  for frame in range(int(detections[:,0].max())):
+    file_name = str(frame).rjust(4, '0') + ".jpg"
+    image_path = PATH_TO_IMAGES + file_name
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image, 'RGBA')
+
+    dets = detections[detections[:,0]==frame, 1:6]
+    dets[:, 3:5] += dets[:, 1:3] # Convert from [x1, y1, width, height] to [x1, y1, x2, y2]
+
+    for d in dets:
+      d = d.astype(np.uint32)
+      c = tuple(colours[d[0]%32, :])
+      draw.rectangle([d[1], d[2], d[3], d[4]], fill=(c + (100,)), outline=c)
+    image.save(PATH_TO_OUTPUT + file_name)
+
+
+def generate_and_save_MATPLOTLIB():
+  import matplotlib.pyplot as plt
+  import matplotlib.patches as patches
+  import matplotlib.image as mpimg
+  from skimage import io
+
+  detections = np.loadtxt(PATH_TO_DETECTIONS_FILE, delimiter=",")
   colours = np.random.rand(32,3)
   plt.ion()
   fig = plt.figure() 
@@ -32,11 +54,11 @@ def generate_and_save():
       d = d.astype(np.uint32)
       ax1.add_patch(patches.Rectangle((d[1],d[2]),d[3],d[4],fill=False,lw=3,ec=colours[d[0]%32,:]))
       ax1.set_adjustable("box-forced")
-
     fig.canvas.flush_events()
     plt.draw()
     plt.savefig(PATH_TO_OUTPUT + file_name)
     ax1.cla()
 
+
 if __name__ == "__main__":
-  draw_images()
+  generate_and_save_PIL()
