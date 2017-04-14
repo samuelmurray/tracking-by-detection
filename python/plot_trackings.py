491d9c9edc82e672a4d20df01d120700f6696510
Samuel Murray <samuel.murray@outlook.com>
2017-04-14 14:25:58 +0900

    Rename and refactor

--- python/plot_trackings.py
+++ python/plot_trackings.py
@@ -8,32 +8,34 @@ import matplotlib.image as mpimg
 from skimage import io
 import time
 
-def draw_images():
-  colours = np.random.rand(32,3) #used only for display
+PATH_TO_DETECTIONS_FILE = "data/detections.txt"
+PATH_TO_IMAGES = "../data/okutama/seq-1_1_10/img1/"
+PATH_TO_OUTPUT = "data/images/"
+
+def generate_and_save():
+  detections = np.loadtxt(PATH_TO_DETECTIONS_FILE, delimiter=",")
+
+  colours = np.random.rand(32,3)
   plt.ion()
   fig = plt.figure() 
   
-
-  #detections = np.loadtxt('../data/results/okutama/img1/seq-1_1_10.txt',delimiter=',') #load detections
-  detections = np.loadtxt("data/detections.txt",delimiter=",") #load detections
-  
   for frame in range(int(detections[:,0].max())):
     ax1 = fig.add_subplot(111, aspect="equal")
-    dets = detections[detections[:,0]==frame,1:6]
-    file_name = f"{str(frame).rjust(4, '0')}.jpg"
-    image_path = f"../data/okutama/seq-1_1_10/img1/{file_name}"
+    file_name = str(frame).rjust(4, '0') + ".jpg"
+    image_path = PATH_TO_IMAGES + file_name
     im = io.imread(image_path)
     plt.imshow(im)
     plt.title("Tracked Targets")
     
+    dets = detections[detections[:,0]==frame,1:6]
     for d in dets:
       d = d.astype(np.uint32)
       ax1.add_patch(patches.Rectangle((d[1],d[2]),d[3],d[4],fill=False,lw=3,ec=colours[d[0]%32,:]))
-      ax1.set_adjustable('box-forced')
+      ax1.set_adjustable("box-forced")
+
     fig.canvas.flush_events()
-    #plt.pause(0.001)
     plt.draw()
-    plt.savefig(f"data/images/{file_name}")
+    plt.savefig(PATH_TO_OUTPUT + file_name)
     ax1.cla()
 
 if __name__ == "__main__":
