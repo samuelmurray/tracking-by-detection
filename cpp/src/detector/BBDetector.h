128b9667355107733481ca39490aee6cc3f274b5
Samuel Murray <samuel.murray@outlook.com>
2017-04-19 12:11:18 +0900

    Remove faulty const declarations

--- cpp/src/detector/BBDetector.h
+++ cpp/src/detector/BBDetector.h
@@ -34,9 +34,9 @@ private:
     void preprocess(const cv::Mat &image, std::vector<cv::Mat> *inputChannels);
 
     std::shared_ptr<caffe::Net<float>> net;
-    const cv::Size inputGeometry;
-    const int numChannels;
-    const cv::Mat mean;
+    cv::Size inputGeometry;
+    int numChannels;
+    cv::Mat mean;
 };
 
 #else // USE_CAFFE
