06bdf9dc3b55db6d16a38cb46a7e0af6ba7a1559
Samuel Murray <samuel.murray@outlook.com>
2017-04-24 16:22:07 +0900

    Require to run this script in folder with images

--- python/generate_video.sh
+++ python/generate_video.sh
@@ -3,5 +3,5 @@ if [[ $# -eq 0 ]] ; then
 	echo 'You need to pass frame rate as argument'
 else 
 	echo 'Creating video with $1 fps'
-	ffmpeg -r $1 -pattern_type glob -i 'output/*.jpg' trk.mp4
+	ffmpeg -r $1 -pattern_type glob -i '*.jpg' trk.mp4
 fi
