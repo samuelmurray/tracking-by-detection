c52af28272e621a4e6778751eb21ce1243bf0d87
Samuel Murray <samuel.murray@outlook.com>
2017-04-24 16:15:44 +0900

    Shell script for generating video from images

---
+++ python/generate_video.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+	echo 'You need to pass frame rate as argument'
+else 
+	echo 'Creating video with $1 fps'
+	ffmpeg -r $1 -pattern_type glob -i 'output/*.jpg' trk.mp4
+fi
