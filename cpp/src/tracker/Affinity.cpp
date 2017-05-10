c27b1024af1f3ef41fc6d0b4338aeae64c33a803
Samuel Murray <samuel.murray@outlook.com>
2017-05-10 14:28:34 +0900

    Added Euclidian distance as measure

--- cpp/src/tracker/Affinity.cpp
+++ cpp/src/tracker/Affinity.cpp
@@ -2,6 +2,10 @@
 
 #include <cmath>
 
+double Affinity::euclDist(const BoundingBox &a, const BoundingBox &b) {
+    return std::sqrt(std::pow(a.cx - b.cx, 2) + std::pow(a.cy - b.cy, 2));
+}
+
 double Affinity::iou(const BoundingBox &a, const BoundingBox &b) {
     double xx1 = std::max(a.x1(), b.x1());
     double yy1 = std::max(a.y1(), b.y1());
@@ -15,20 +19,20 @@ double Affinity::iou(const BoundingBox &a, const BoundingBox &b) {
 }
 
 double Affinity::linCost(const BoundingBox &a, const BoundingBox &b) {
-    double position = std::sqrt(std::pow(a.cx - b.cx, 2) + std::pow(a.cy - b.cy, 2));
-    double shape = std::sqrt(std::pow(a.width - b.width, 2) + std::pow(a.height - b.height, 2));
-    return position * shape;
+    double positionCost = std::sqrt(std::pow(a.cx - b.cx, 2) + std::pow(a.cy - b.cy, 2));
+    double shapeCost = std::sqrt(std::pow(a.width - b.width, 2) + std::pow(a.height - b.height, 2));
+    return positionCost * shapeCost;
 }
 
 double Affinity::expCost(const BoundingBox &a, const BoundingBox &b) {
     // It is expected that a is detection and b is prediction
-    double w1 = 0.5;
-    double w2 = 1.5;
-    double position = std::exp(-w1 * (
+    double positionWeight = 0.5;
+    double shapeWeight = 1.5;
+    double positionCost = std::exp(-positionWeight * (
             std::pow((a.cx - b.cx) / a.width, 2) +
             std::pow((a.cy - b.cy) / a.height, 2)));
-    double shape = std::exp(-w2 * (
+    double shapeCost = std::exp(-shapeWeight * (
             std::abs(a.width - b.width) / (a.width + b.width) +
             std::abs(a.height - b.height) / (a.height + b.height)));
-    return position * shape;
+    return positionCost * shapeCost;
 }
--- cpp/src/tracker/Affinity.h
+++ cpp/src/tracker/Affinity.h
@@ -7,6 +7,13 @@
 class Affinity {
 public:
     /**
+     * Euclidian distance
+     * Small value indicates similarity
+     */
+    static double euclDist(const BoundingBox &a, const BoundingBox &b);
+
+    /**
+     * Intersection over union
      * Large value indicates similarity
      * From: Simple Online and Realtime Tracking
      * (Alex Bewley, Zongyuan Ge, Lionel Ott, Fabio Ramos, Ben Upcroft)
@@ -14,6 +21,7 @@ public:
     static double iou(const BoundingBox &a, const BoundingBox &b);
 
     /**
+     * Combination of linear similarity in position and shape
      * Small value indicates similarity
      * From: Online multi-target tracking with strong and weak detections
      * (Ricardo Sanchez-Matilla, Fabio Poiesi, Andrea Cavallaro Centre)
@@ -21,6 +29,7 @@ public:
     static double linCost(const BoundingBox &a, const BoundingBox &b);
 
     /**
+     * Combination of exponential similarity in position and shape
      * Large value indicates similarity
      * From: POI: Multiple Object Tracking with High Performance Detection and Appearance Feature
      * (Fengwei Yu, Wenbo Li, Quanquan Li, Yu Liu, Xiaohua Shi, Junjie Yan)
